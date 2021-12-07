04215f5539e11b632f4596ce862cc29026ac7654
Yassino1-dev <yassineelmoustachir@gmail.com>
2021-12-07 13:08:56 +0100

    fourth commit

---
+++ ASPCRUDProject/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUDTEST.Startup))]
+namespace CRUDTEST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
+
+
