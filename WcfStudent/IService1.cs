63197196419d2ecc2f152468f1558658ab39a216
Yassino1-dev <yassineelmoustachir@gmail.com>
2021-12-07 12:57:02 +0100

    first commit

---
+++ WcfStudent/IService1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfStudent
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [FaultContract(typeof(ExceptionMessage))]
+        List<Student> GetStudents();
+
+        [OperationContract]
+        [FaultContract(typeof(ExceptionMessage))]
+        void AddStudents(Student student);
+
+        [OperationContract]
+        [FaultContract(typeof(ExceptionMessage))]
+        void DeleteStudent(long StudentId);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Student
+    {
+        [DataMember]
+        public long StudentId;
+        [DataMember]
+        public string FirstName;
+        [DataMember]
+        public string LastName;
+        [DataMember]
+        public string RegisterNo;
+        [DataMember]
+        public string Department;
+    }
+
+    [DataContract]
+    public class ExceptionMessage
+    {
+        private string infoExceptionMessage;
+        public ExceptionMessage(string Message)
+        {
+            this.infoExceptionMessage = Message;
+        }
+        [DataMember]
+        public string errorMessageOfAction
+        {
+            get { return this.infoExceptionMessage; }
+            set { this.infoExceptionMessage = value; }
+        }
+    }
+}
---
+++ WcfStudent/Service1.svc.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfStudent
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public static String ConnectionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+        public List<Student> GetStudents()
+        {
+            List<Student> studentList = new List<Student>();
+            DataTable resourceTable = new DataTable();
+            SqlDataReader resultReader = null;
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            SqlCommand command = new SqlCommand("GetStudent", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                connection.Open();
+                resultReader = command.ExecuteReader();
+                resourceTable.Load(resultReader);
+                resultReader.Close();
+                connection.Close();
+                studentList = (from DataRow dr in resourceTable.Rows
+                               select new Student()
+                               {
+                                   StudentId = Convert.ToInt64(dr["StudentId"]),
+                                   FirstName = dr["FirstName"].ToString(),
+                                   LastName = dr["LastName"].ToString(),
+                                   RegisterNo = dr["RegisterNo"].ToString(),
+                                   Department = dr["Department"].ToString()
+                               }).ToList();
+            }
+            catch (Exception exception)
+            {
+                if (resultReader != null || connection.State == ConnectionState.Open)
+                {
+                    resultReader.Close();
+                    connection.Close();
+                }
+                throw new FaultException<ExceptionMessage>(new ExceptionMessage(exception.Message));
+            }
+            return studentList;
+        }
+
+        public void AddStudents(Student student)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("AddStudent", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add(new SqlParameter("@StudentId", student.StudentId));
+                    command.Parameters.Add(new SqlParameter("@FirstName", student.FirstName));
+                    command.Parameters.Add(new SqlParameter("@LastName", student.LastName));
+                    command.Parameters.Add(new SqlParameter("@RegisterNo", student.RegisterNo));
+                    command.Parameters.Add(new SqlParameter("@Department", student.Department));
+                    object result = command.ExecuteScalar();
+                    connection.Close();
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new FaultException<ExceptionMessage>(new ExceptionMessage(exception.Message));
+            }
+        }
+
+        public void DeleteStudent(long StudentId)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("DeleteStudent", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add(new SqlParameter("@StudentId", StudentId));
+                    int result = command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new FaultException<ExceptionMessage>(new ExceptionMessage(exception.Message));
+            }
+        }
+    }
+}
